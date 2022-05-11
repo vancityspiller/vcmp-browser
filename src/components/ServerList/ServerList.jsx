e89a519604a54ef5611d845ce6f17db8b3430165
Spiller <xiwcoc@gmail.com>
2022-05-11 17:38:14 +0530

    Implement searching

--- src/components/ServerList/ServerList.jsx
+++ src/components/ServerList/ServerList.jsx
@@ -1,7 +1,11 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useMemo } from 'react';
+import { Input, InputGroup } from 'rsuite';
 import ServerInfoDrawer from '../ServerInfoDrawer/ServerInfoDrawer';
 
 // ========================================================= //
+
+import ReloadIcon from '@rsuite/icons/Reload';
+import CloseIcon from '@rsuite/icons/Close';
 
 import './serverlist.less';
 
@@ -33,7 +37,8 @@ const data = [{
     ping: '140',
     numPlayers: 5,
     maxPlayers: 100,
-    ip: "3"
+    ip: "3",
+    players: []
 },
 {
     serverName: 'Rob The Vehicle (IV)',
@@ -41,7 +46,8 @@ const data = [{
     ping: '140',
     numPlayers: 5,
     maxPlayers: 100,
-    ip: "4"
+    ip: "4",
+    players: []
 },
 {
     serverName: 'Rob The Vehicle (IV)',
@@ -49,7 +55,8 @@ const data = [{
     ping: '140',
     numPlayers: 5,
     maxPlayers: 100,
-    ip: "5"
+    ip: "5",
+    players: []
 },
 {
     serverName: 'Rob The Vehicle (IV)',
@@ -57,7 +64,8 @@ const data = [{
     ping: '140',
     numPlayers: 5,
     maxPlayers: 100,
-    ip: "6"
+    ip: "6",
+    players: []
 },
 {
     serverName: 'Rob The Vehicle (IV)',
@@ -65,7 +73,8 @@ const data = [{
     ping: '140',
     numPlayers: 5,
     maxPlayers: 100,
-    ip: '7'
+    ip: '7',
+    players: []
 },
 {
     serverName: 'Rob The Vehicle (IV)',
@@ -73,7 +82,8 @@ const data = [{
     ping: '140',
     numPlayers: 5,
     maxPlayers: 100,
-    ip: '8'
+    ip: '8',
+    players: []
 },
 {
     serverName: 'Rob The Vehicle (IV)',
@@ -81,7 +91,8 @@ const data = [{
     ping: '140',
     numPlayers: 5,
     maxPlayers: 100,
-    ip: '9'
+    ip: '9',
+    players: []
 },
 {
     serverName: 'Polski LCS-DM (Liberty City Stories) by LU-DM Team - discord.gg/PFwem6J',
@@ -89,7 +100,8 @@ const data = [{
     ping: '140',
     numPlayers: 5,
     maxPlayers: 100,
-    ip: '10'
+    ip: '10',
+    players: []
 }];
 
 // --------------------------------------------------------- //
@@ -97,9 +109,45 @@ const data = [{
 function ServerList() {
 
     const [rawData, setRawData] = useState([...data]);
-    const [rows, setRows] = useState([...rawData]);
-
     const [selected, setSelected] = useState(null);
+
+    const [search, setSearch] = useState('');
+
+    const handleSearch = (value) => {
+        setSearch(value.trim());
+    }
+
+    const rows = useMemo(() => {
+
+        let borrowed = [...rawData];
+
+        if(search.trim() !== '') {
+            borrowed = borrowed.filter(server => {
+
+                const searchTerm = search.toLowerCase();
+
+                if(server.serverName.toLowerCase().indexOf(searchTerm) !== -1)
+                    return true;
+
+                if(server.ip.toLowerCase().indexOf(searchTerm) !== -1)
+                    return true;
+
+                let playerSearch = false;
+                
+                for(let i = 0 ; i < server.players.length ; i++) {
+                    if(server.players[i].toLowerCase().indexOf(searchTerm) !== -1) {
+                        playerSearch = true;
+                        break;
+                    }
+                }
+
+                if(playerSearch === true) return true;
+                return false;
+            });
+        }
+
+        return borrowed;
+    }, [rawData, search]);
 
     // --------------------------------------------------------- //
     // for drawer
@@ -143,6 +191,25 @@ function ServerList() {
                 
             </div>
             
+            <div className='srvBarWrapper'>
+                <InputGroup>
+                    <InputGroup.Button appearance='primary'>
+                        <ReloadIcon />
+                    </InputGroup.Button>
+
+                    <Input 
+                        placeholder='Search' 
+                        size='sm' 
+                        value={search}
+                        onChange={handleSearch}
+                    />
+
+                    <InputGroup.Button appearance='primary' onClick={() => handleSearch('')}>
+                        <CloseIcon />
+                    </InputGroup.Button>
+                </InputGroup>
+            </div>
+
             <ServerInfoDrawer open={drawerOpen} handleClose={handleDrawerClose} data={selected}/>
         </React.Fragment>
     );
