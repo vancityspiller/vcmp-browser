c8034715fdd4b597a28498bb6caabc190980cfe6
Spiller <xiwcoc@gmail.com>
2022-05-26 09:13:58 +0530

    Add window shadows

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,9 @@
   windows_subsystem = "windows"
 )]
 
+use window_shadows::set_shadow;
+use tauri::Manager;
+
 mod server;
 mod extract;
 mod launch;
@@ -12,6 +15,11 @@ mod download;
 fn main() {
   tauri::Builder::default()
     .invoke_handler(tauri::generate_handler![server::info, extract::extract7z, launch::launch_game, rpc::discord_presence, download::downloadFiles])
+    .setup(|app| {
+      let window = app.get_window("main").unwrap();
+      set_shadow(&window, true).expect("Unsupported platform!");
+      Ok(())
+    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
