2123209c9176eca087228db462257bd526347fd8
Spiller <xiwcoc@gmail.com>
2022-06-07 12:57:36 +0530

    Fix indentation

--- src-tauri/src/extract.rs
+++ src-tauri/src/extract.rs
@@ -10,7 +10,7 @@ use std::fs::create_dir;
 // ------------------------------------------------------------------------------------------------ //
 
 fn u2w(u8str: &str) -> Vec<u16> {
-	OsStr::new(u8str).encode_wide().chain(Some(0).into_iter()).collect::<Vec<_>>()
+    OsStr::new(u8str).encode_wide().chain(Some(0).into_iter()).collect::<Vec<_>>()
 }
 
 fn w2u(wstr: *const u16) -> String {
@@ -28,34 +28,34 @@ fn w2u(wstr: *const u16) -> String {
 pub async fn extract7z(path: String, dest: String) -> Result<String, String>{
     unsafe {
 
-		rust7z::init7z();
+        rust7z::init7z();
         
-		let k = u2w(&path);
-		let file_count = rust7z::open(k.as_ptr()).file_count;
-		let mut vec = Vec::new();
+        let k = u2w(&path);
+        let file_count = rust7z::open(k.as_ptr()).file_count;
+        let mut vec = Vec::new();
         
-		for i in 0..file_count {
-			let file = rust7z::getFileInfo(i);
-			let fname = w2u(file.path);
+        for i in 0..file_count {
+            let file = rust7z::getFileInfo(i);
+            let fname = w2u(file.path);
 
             if file.size == 0 {
 
-				create_dir(format!("{}\\{}", &dest, &fname)).unwrap();
+            create_dir(format!("{}\\{}", &dest, &fname)).unwrap();
                 continue;
             }
 
-			let buf = vec![0; file.size as usize];
-			rust7z::extractToBuf(buf.as_ptr(), &i, 1);
-			let output = File::create(&(dest.to_owned() + &fname.to_string())).unwrap();
-			let mut writer = BufWriter::new(output);
-			writer.write(&buf).unwrap();
-			writer.flush().unwrap();
-			vec.push(i);
-		}
+            let buf = vec![0; file.size as usize];
+            rust7z::extractToBuf(buf.as_ptr(), &i, 1);
+            let output = File::create(&(dest.to_owned() + &fname.to_string())).unwrap();
+            let mut writer = BufWriter::new(output);
+            writer.write(&buf).unwrap();
+            writer.flush().unwrap();
+            vec.push(i);
+        }
 
-		rust7z::close();
+        rust7z::close();
         Ok("".into())
-	}
+    }
 }
 
 // ------------------------------------------------------------------------------------------------ //
